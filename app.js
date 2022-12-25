5d93fab2138fb1cf0867127da2f905794b8999c8
Polina Andreyanova <andreyanovalina@yandex.ru>
2022-12-25 20:53:31 +0300

    feat: исправить ошибки в реализации валидации приходящих на сервер запросов

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string(),
   }).unknown(true),
 }), createUser);
 
