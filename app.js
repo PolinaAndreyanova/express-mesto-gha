179747252658968646fb3a974e4db07e10d69369
Polina Andreyanova <andreyanovalina@yandex.ru>
2022-12-26 13:27:34 +0300

    feat: исправить ошибки в реализации валидации приходящих на сервер запросов

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({ message: statusCode === 500 ? 'Произошла ошибка' : message });
-  next();
 });
 
 mongoose.connect('mongodb://127.0.0.1/mestodb', { useNewUrlParser: true })
